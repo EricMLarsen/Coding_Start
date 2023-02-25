38884159d4b1036c6e6bc60fea15e6fcab51cba7
Eric_Larsen <Eric.Larsen.M@gmail.com>
2023-02-24 23:54:44 -0700

    updating files for lesson

---
+++ Eric_Files/helloworld.py
@@ -0,0 +1,24 @@
+## Lets get the users name
+
+name = input("What is your name?\n")
+
+## get a quest
+
+quest = input("\nWhat is your quest?\n")
+
+## get a favorite color
+
+favcolor = input("\nWhat is your Favorite Color?\n")
+
+## get their favorite number
+
+favnum = input("\nWhat is your Favorite Number?\n")
+
+## lets use this information for something semi useful
+
+#first statement
+print(f"\n{name} you look nice today")
+print(f"\nI don't like your {quest}, I think it is dumb")
+print(f"\nWho in the world likes {favcolor}? You should seek help")
+print(f"\nI can dig {favnum} as a favorite number")
+print("\nYou can cross my bridge")
