6efbd4cf73c989d64ef030ec67d40e90aa3647ce
Elli Larsen <ellibelli10@gmail.com>
2024-02-09 22:15:03 -0700

    my code

--- Elli_Files/Elli_hello_world.py
+++ Elli_Files/Elli_hello_world.py
@@ -3,3 +3,13 @@
 #lets get the user's name
 name = input("What is your name hot stuff?\n")
 print(f"hello world!\n {name} doesn't know what she is doing")
+
+## figure out how to math
+number1 = float(input("Choose a number b*tch"))
+number2 = float(input("Anotha!"))
+multiplication_multiplication_result = number1 ** number2
+print(multiplication_multiplication_result)
+
+
+
+
--- hey.py
+++ hey.py
@@ -1,3 +1,4 @@
 print("Hello World"
         )
 print("This is coding in vim, a terminal sub command for text editing")
+print("Don't go chasing waterfalls")
